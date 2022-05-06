05e25f67e7d93bb51a51164e4eb21de08d4e7649
tmrk <tamas.marki@gmail.com>
2022-05-06 10:06:15 +0200

    Temporary-2 is added

--- Asset_Tracking_20220504/Program.cs
+++ Asset_Tracking_20220504/Program.cs
@@ -102,6 +102,8 @@ namespace Asset_Tracking_20220504
             // new comment
             // second comment
             // This code is for temporary branch
+            // This code is for temporary-2 branch
+
             //width = (width != 0) ? width : Console.WindowWidth - (margin * 2) - (hPadding * 2) - 2;
             content = TextAlign(content, width, textAlign);
             //margin = (margin != 0) ? margin : (Console.WindowWidth - margin - width - hPadding) / 2;
